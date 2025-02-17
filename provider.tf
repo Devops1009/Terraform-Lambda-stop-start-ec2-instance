aa9a19967428bab110f77a7bfd8d04d3f7ab81a9
indar99 <47505264+indar99@users.noreply.github.com>
2025-02-17 14:54:28 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
+
 provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIASPKS7RE3ZFQ"
-  secret_key = "pNBTnn2i8g2ufOwdQ9R2t2CY9tXSS62EIZOzB6"
+  region     = "us-east-1"
+  access_key = "AKIASKHPKS7RE3ZFQOX3"
+  secret_key = "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"
 }
